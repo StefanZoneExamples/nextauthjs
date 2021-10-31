690939020bafcbd52ee832cf71c76e5b48dbbfd7
Stefan Kühnel <git@stefankuehnel.com>
2021-10-31 16:17:13 +0100

    Update Privacy Policy.

--- pages/privacy/index.tsx
+++ pages/privacy/index.tsx
@@ -13,8 +13,8 @@ const Index: NextPage = () => {
                 <h1>Privacy Policy</h1>
                 <p>
                     Data provided to this site is exclusively used to support signing in and is not
-                    passed to any third party services, other than via SMTP or OAuth for the
-                    purposes of authentication.
+                    passed to any third party services, other than via OAuth for the purposes of
+                    authentication.
                 </p>
             </Layout>
         </>
