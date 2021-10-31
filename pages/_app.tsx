2459ff07ee84e427d0d49dd9102af636686eed1d
Stefan Kühnel <git@stefankuehnel.com>
2021-10-31 18:42:57 +0100

    Import `normalize.css`.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@fontsource/source-serif-pro/latin.css';
 import { Provider } from 'next-auth/client';
 import type { AppProps } from 'next/app';
+import 'normalize.css';
 import 'styles/globals.css';
 
 function App({ Component, pageProps }: AppProps) {
