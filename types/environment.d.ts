81436adc086159516992e8b57c177907682921db
Stefan Kühnel <git@stefankuehnel.com>
2021-11-18 02:59:50 +0100

    Add `GOOGLE_CLIENT_EMAIL` and `GOOGLE_PRIVATE_KEY`.

--- types/environment.d.ts
+++ types/environment.d.ts
@@ -2,6 +2,8 @@ namespace NodeJS {
     interface ProcessEnv extends NodeJS.ProcessEnv {
         GOOGLE_ID: string;
         GOOGLE_SECRET: string;
+        GOOGLE_CLIENT_EMAIL: string;
+        GOOGLE_PRIVATE_KEY: string;
         SECRET: string;
     }
 }
